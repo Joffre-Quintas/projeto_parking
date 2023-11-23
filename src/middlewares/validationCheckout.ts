c16dcac543ef840f8b1d4cd0039819553a92a34f
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-11-22 21:09:06 -0300

    feat: implementado desafios 3 e 4

--- src/middlewares/validationCheckout.ts
+++ src/middlewares/validationCheckout.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express"
 import { ParkingRepository } from "../repositories/ParkingRepository"
 
 export default function valdiationCheckout(req:Request, res: Response, next: NextFunction) {
-  const { pos, plateCar, carModel } = req.body
+  const { plateCar } = req.body
   const parkingRepository = new ParkingRepository()
   const car = parkingRepository.listAllSlot().find(car => car.plateCar === plateCar)
   const plateRegex = /[A-Z]{3}[0-9]{4}/
@@ -10,8 +10,8 @@ export default function valdiationCheckout(req:Request, res: Response, next: Nex
   if(!plateRegex.test(plateCar)) {
     return res.status(404).json({ message: `Informe corretamente a placa. Formato: XXX0000`})
   }
-  if(car) {
-    return res.status(404).json({ message: `Nao existe carro estaciodano com a placa ${plateCar}`})
+  if(!car) {
+    return res.status(404).json({ message: `Nao existe carro estacionado com a placa ${plateCar}`})
   }
   
   next()
--- src/middlewares/validationPlateCheckout.ts
+++ src/middlewares/validationPlateCheckout.ts
@@ -3,15 +3,19 @@ import { ParkingRepository } from "../repositories/ParkingRepository"
 
 export default function validationPlateCheckout(req:Request, res: Response, next: NextFunction) {
     const plateCar = req.params.plate
+    console.log(plateCar)
     const parkingRepository = new ParkingRepository()
-    const car = parkingRepository.listAllSlot().find(car => car.plateCar === plateCar)
+    const data = parkingRepository.listAllSlot()
+    console.log(data)
+    const existCarPlate = data.find(car => car.plateCar === plateCar)
+    console.log(existCarPlate)
     const plateRegex = /[A-Z]{3}[0-9]{4}/
 
     if(!plateRegex.test(plateCar)) {
         return res.status(404).json({ message: `Informe corretamente a placa. Formato: XXX0000`})
     }
-    if(car) {
-        return res.status(404).json({ message: `Nao existe carro estaciodano com a placa ${plateCar}`})
+    if(!existCarPlate) {
+        return res.status(404).json({ message: `Nao existe carro estacionado com a placa ${plateCar}`})
     }
 
     next()
